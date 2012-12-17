0025fbb44bb0341ad6776a74c11089c887d4d244
Glenn Ruehle <gruehle@gmail.com>
2012-12-16 19:58:34 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## Markdown Preview
+
+A [Brackets](https://github.com/adobe/brackets) extension that provides a live preview of markdown documents. 
+
+### Installation
+
+* [Download the Zip](https://github.com/gruehle/MarkdownPreview/downloads) and unzip it; or clone this repo on GitHub.
+* From Brackets, choose _Help > Show Extensions Folder_. This will open the extensions folder.
+* Drag the unzipped (or cloned) folder into the `user` directory.
+* Restart Brackets.
+
+### How To Use
+When a markdown document (with extension ".md" or ".markdown") is open, a markdown icon is shown in the 
+toolbar at the top of the Brackets window. Click this icon to open the preview panel. The panel can be 
+resized vertically.
+
+The preview is updated as you edit the document. You can hover over links to see the href in a tooltip.
+
+### Credits
+This extension uses the following open source components:
+
+* [Marked](https://github.com/chjj/marked) - A markdown parser written in JavaScript
+* [markdown-css-themes](https://github.com/jasonm23/markdown-css-themes) - This extension uses the "Swiss" theme
+* [markdown-mark](https://github.com/dcurtis/markdown-mark) - The icon used in the toolbar
---
+++ main.js
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2012 Glenn Ruehle
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true,  regexp: true, indent: 4, maxerr: 50 */
+/*global define, brackets, $, window, PathUtils, marked */
+
+define(function (require, exports, module) {
+    "use strict";
+    
+    require("marked");
+
+    // Brackets modules
+    var AppInit             = brackets.getModule("utils/AppInit"),
+        DocumentManager     = brackets.getModule("document/DocumentManager"),
+        EditorManager       = brackets.getModule("editor/EditorManager"),
+        ExtensionUtils      = brackets.getModule("utils/ExtensionUtils"),
+        FileUtils           = brackets.getModule("file/FileUtils"),
+        NativeFileSystem    = brackets.getModule("file/NativeFileSystem").NativeFileSystem,
+        Resizer             = brackets.getModule("utils/Resizer"),
+        StringUtils         = brackets.getModule("utils/StringUtils");
+
+    // Local modules
+    var panelHTML   = require("text!panel.html");
+    
+    // jQuery objects
+    var $icon,
+        $iframe,
+        $panel;
+    
+    // Other vars
+    var currentDoc,
+        visible = false,
+        realVisibility = false;
+    
+    function _loadDoc(doc, preserveScrollPos) {
+        if (doc && visible && $iframe) {
+            var bodyText = marked.parse(doc.getText()),
+                scrollPos = 0;
+            
+            if (preserveScrollPos) {
+                scrollPos = $iframe.contents()[0].body.scrollTop;
+            }
+                        
+            // Remove link hrefs
+            bodyText = bodyText.replace(/href=\"([^\"]*)\"/g, "title=\"$1\"");
+            var htmlSource = "<html><head>";
+            htmlSource += "<link href='" + require.toUrl("./markdown.css") + "' rel='stylesheet'></link>";
+            htmlSource += "</head><body onload='document.body.scrollTop=" + scrollPos + "'>";
+            htmlSource += bodyText;
+            htmlSource += "</body></html>";
+            $iframe.attr("srcdoc", htmlSource);
+        }
+    }
+    
+    function _documentChange(e) {
+        _loadDoc(e.target, true);
+    }
+    
+    function _resizeIframe() {
+        if (visible && $iframe) {
+            var iframeWidth = $panel.innerWidth() - 40;
+            $iframe.attr("width", iframeWidth + "px");
+        }
+    }
+    
+    function _setPanelVisibility(isVisible) {
+        if (isVisible === realVisibility) {
+            return;
+        }
+        
+        realVisibility = isVisible;
+        if (isVisible) {
+            if (!$panel) {
+                $panel = $(panelHTML);
+                $iframe = $panel.find("#panel-markdown-preview-frame");
+                
+                $panel.insertBefore("#status-bar");
+
+                Resizer.makeResizable($panel.get(0), "vert", "top", 100, false);
+                $panel.on("panelResizeUpdate", function (e, newSize) {
+                    $iframe.attr("height", newSize - 20);
+                });
+                $iframe.attr("height", $panel.height() - 20);
+                window.setTimeout(_resizeIframe);
+            }
+            _loadDoc(DocumentManager.getCurrentDocument());
+            $panel.show();
+        } else {
+            $panel.hide();
+        }
+        EditorManager.resizeEditor();
+    }
+
+    function _currentDocChangedHandler() {
+        var doc = DocumentManager.getCurrentDocument(),
+            ext = doc ? PathUtils.filenameExtension(doc.file.fullPath).toLowerCase() : "";
+        
+        if (currentDoc) {
+            $(currentDoc).off("change", _documentChange);
+            currentDoc = null;
+        }
+        
+        if (doc && /md|markdown/.test(ext)) {
+            currentDoc = doc;
+            $(currentDoc).on("change", _documentChange);
+            $icon.css({display: "inline-block"});
+            _setPanelVisibility(visible);
+            _loadDoc(doc);
+        } else {
+            $icon.css({display: "none"});
+            _setPanelVisibility(false);
+        }
+    }
+    
+    function _toggleVisibility() {
+        visible = !visible;
+        _setPanelVisibility(visible);
+    }
+    
+    // Insert CSS for this extension
+    ExtensionUtils.loadStyleSheet(module, "MarkdownPreview.css");
+    
+    // Add toolbar icon 
+    $icon = $("<a>")
+        .attr({
+            id: "markdown-preview-icon",
+            href: "#"
+        })
+        .css({
+            display: "none"
+        })
+        .click(_toggleVisibility)
+        .insertAfter($("#toolbar-go-live"));
+    
+    // Add a document change handler
+    $(DocumentManager).on("currentDocumentChange", _currentDocChangedHandler);
+    
+    // currentDocumentChange is *not* called for the initial document. Use
+    // appReady() to set initial state.
+    AppInit.appReady(function () {
+        _currentDocChangedHandler();
+    });
+    
+    $(window).on("resize", _resizeIframe);
+
+});
